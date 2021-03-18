6301b7883ca2eae150724d6dbb62dbbc4da79b90
1pluszone <olalekanopadere@gmail.com>
2021-03-18 20:30:52 +0100

    details page

--- lib/view/widgets/each_comic.dart
+++ lib/view/widgets/each_comic.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:marvel_comics/models/by_character_model.dart';
+import 'package:intl/intl.dart';
 
 class EachComic extends StatelessWidget {
   final Results eachComic;
@@ -7,16 +8,45 @@ class EachComic extends StatelessWidget {
 
   EachComic({@required this.eachComic, @required this.comicImage});
 
+  final List<Widget> childrenList = [];
+  final DateFormat formatter = DateFormat('MMMM dd, yyyy');
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          comicImage,
-          Text(eachComic.title ?? ""),
-          Text(eachComic.description ?? "No description included"),
-        ],
+    String url = "${eachComic.images[0].path}.${eachComic.images[0].extension}";
+
+    childrenList.add(Text(eachComic.title ?? ""));
+    childrenList.add(Image.network(url));
+    childrenList.add(Text(eachComic.description ?? "No description included"));
+    childrenList.add(Text("Published:"));
+    childrenList
+        .add(Text(formatter.format(DateTime.parse(eachComic.dates[0].date))));
+    creators();
+    childrenList.add(Wrap(
+      children: [
+        Text("price: \$"),
+        Text(eachComic.prices[0].price.toString()),
+      ],
+      //
+    ));
+
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: childrenList,
+        ),
       ),
     );
+  }
+
+  void creators() {
+    for (Items creators in eachComic.creators.items) {
+      childrenList.add(Column(
+        children: [
+          Text(creators.role),
+          Text(creators.name),
+        ],
+      ));
+    }
   }
 }
